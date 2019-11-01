517674c315efaba3e0e7f4a44c7003717e62e3fa
ClemenceYi <33429380+ClemenceYi@users.noreply.github.com>
2019-11-01 00:13:36 -0700

    Added OpenCV version 3.7.4

--- app/src/main/java/com/clemencism/sleepwell/GetTiredness.java
+++ app/src/main/java/com/clemencism/sleepwell/GetTiredness.java
@@ -28,25 +28,6 @@ public class GetTiredness {
 
 
     public static void main(String[] args) {
-        String filePath;
-        String casadePath;
-        CascadeClassifier faceCascade = new CascadeClassifier();
-        CascadeClassifier classifier;
-
-
-        casadePath = "cascades/data/haarcascade_frontalface_alt2.xml";
-        faceCascade.load(casadePath);
-
-        Imgcodecs imageCodecs = new Imgcodecs();
-        Mat img = imageCodecs.imread(filePath);
-
-        Mat imggray = new Mat();
-        Imgproc.cvtColor(img, imggray, Imgproc.COLOR_BGR2GRAY);
-
-
-        faceCascade.detectMultiScale(imggray, faces, 1.2, 6);
-
-
 
     }
 
